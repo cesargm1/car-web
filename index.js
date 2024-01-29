90a8f64511734fa99905a7b5c861c741f6533b21
cesar <cg.gtav@gmail.com>
2024-01-29 13:34:11 +0100

    issue when open menu

---
+++ index.js
@@ -0,0 +1,9 @@
+function openMenu(){
+    const menu = document.querySelector("nav");
+    menu.classList.add("open");
+}
+
+    function closeMenu(){
+        const menu = document.querySelector("nav");
+        menu.classList.remove("open");
+    }
